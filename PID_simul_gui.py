d39153458f138a5ef7dec2beaee47798dceb5604
kimdohoon <intwwf@gmail.com>
2022-01-22 15:03:37 +0900

    20220122_commit

--- PID_simul_gui.py
+++ PID_simul_gui.py
@@ -25,7 +25,11 @@ class MainWindow(QMainWindow, form_class):
         
         self.pid_input.clicked.connect(self.inComp)
         self.pb_show.clicked.connect(self.pid_calc)
-        #self.pb_clear.clicked.connect(self.logClear)
+        self.pb_textClear.clicked.connect(self.textClear)
+        
+        self.rb_type1.clicked.connect(self.outSelect)
+        self.rb_type2.clicked.connect(self.outSelect)
+        self.rb_type3.clicked.connect(self.outSelect)
         
         self.fig = plt.figure()
         self.canvasMain = FigureCanvas(self.fig)
@@ -34,6 +38,15 @@ class MainWindow(QMainWindow, form_class):
         
     def cmbSet(self):
         print(self.cmb_contType.currentIndex())
+        
+    def outSelect(self):
+        
+        if self.rb_type1.isChecked():
+            self.outType = 1
+        elif self.rb_type2.isChecked():
+            self.outType = 2
+        elif self.rb_type3.isChecked():
+            self.outType =3      
         
     def inComp(self):
         
@@ -45,17 +58,22 @@ class MainWindow(QMainWindow, form_class):
         self.mv_plt = []
         #self.disp_id = []
         
-        self.kp_val = self.kp_input.text()
-        self.ki_val = self.ki_input.text()
-        self.kd_val = self.kd_input.text()
-        self.sp_val = self.sp_input.text()
+        self.kp_val_s = self.kp_input.text()
+        self.ki_val_s = self.ki_input.text()
+        self.kd_val_s = self.kd_input.text()
+        self.sp_val_s = self.sp_input.text()
         
-        self.kp_val = float(self.kp_val)
-        self.ki_val = float(self.ki_val)
-        self.kd_val = float(self.kd_val)
-        self.sp_val = float(self.sp_val)
+        self.outType_s = str(self.outType)
+        
+        self.kp_val = float(self.kp_val_s)
+        self.ki_val = float(self.ki_val_s)
+        self.kd_val = float(self.kd_val_s)
+        self.sp_val = float(self.sp_val_s)
         
         self.contType = self.cmb_contType.currentIndex()
+        
+        self.textEdit.append("입력값 \n Kp : {} | Ki : {} | kd : {}  | 목표값 : {}".format(self.kp_val_s, self.ki_val_s, self.kd_val_s, self.sp_val_s))
+        self.textEdit.append("제어기 출력 Type{}이 선택되었습니다".format(self.outType_s))
         
         print("Kp : {} | Ki : {} | kd : {}".format(self.kp_val, self.ki_val, self.kd_val))
         print(self.contType)
@@ -66,9 +84,9 @@ class MainWindow(QMainWindow, form_class):
         
         
         
-    def logClear(self):
+    def textClear(self):
         
-        self.disp_log.clear()
+        self.textEdit.clear()
         
     def pid_calc(self):
         
@@ -76,8 +94,16 @@ class MainWindow(QMainWindow, form_class):
             
             if self.contType == 1:
                 self.u_1 = self.u_2 + self.kp_val * (self.err_1 - self.err_2) + self.ki_val * self.err_1 + self.kd_val * (self.err_1 - 2 * self.err_2 + self.err_3)
-                self.y_0 = 0.77 * self.y_1 + 0.322 * self.u_1 + 0.111 * self.u_2
-                #self.y_0 = 1 * self.y_1 + 1 * self.u_1 + 1 * self.u_2
+                if self.outType == 1:
+                    self.y_0 = 0.5 * self.u_1
+                    
+                elif self.outType == 2:
+                     self.y_0 = 1 * self.y_1 + 1 * self.u_1 + 1 * self.u_2
+                     
+                elif self.outType == 3:
+                    self.y_0 = 0.77 * self.y_1 + 0.322 * self.u_1 + 0.111 * self.u_2
+                
+                
                 self.err = self.sp_val - self.y_0
                 self.py = self.y_0
                 
@@ -90,20 +116,11 @@ class MainWindow(QMainWindow, form_class):
             self.u_2 = self.u_1
             self.y_1 = self.y_0
             
-            print("횟수:", i, "목표값:", self.sp_val, "제어기기출력:", self.u_1, "플랜트 출력:", self.y_0)
-            
             self.id_plt.append(i)
             self.plant_plt.append(self.y_0)
             self.mv_plt.append(self.u_1)
             
-            self.disp_id = str(i)
-            self.disp_plant_plt = str(self.y_0)
-            self.disp_mv_plt = str (self.u_1)
-            self.disp_log = str(self.disp_plant_plt + self.disp_mv_plt)
-            
-            #self.disp_log.insertItem(self.id+plt, self.disp_log)
-            #self.disp_log.insertItem(i, self.disp_plant_plt)
-            #print(self.disp_plant_plt)
+            self.textEdit.append("[{}]  목표값 : {} | 제어출력 : {} | 플랜트출력 : {}".format(i, self.sp_val, self.u_1, self.y_0))
             
         plt.cla()
         plt.plot(self.id_plt, self.plant_plt)
